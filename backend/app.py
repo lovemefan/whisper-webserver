eacb31b1a259171ba9e20d109f8fc90f09ca2a40
lovemefan <lovemefan@outlook.com>
2023-01-07 17:14:21 +0800

    init

---
+++ backend/app.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+# @FileName  :app.py
+# @Time      :2023/1/7 17:02
+# @Author    :lovemefan
+# @email     :lovemefan@outlook.com
---
+++ backend/service/WhisperAsrService.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+# @FileName  :WhisperAsrService.py
+# @Time      :2023/1/7 17:01
+# @Author    :lovemefan
+# @email     :lovemefan@outlook.com
