f3be8f9cd838640f26a65e7995c204cd3bb3f322
lovemefan <lovemefan@outlook.com>
2023-01-08 14:00:49 +0800

    add swagger api and handle Cross-origin

--- backend/app.py
+++ backend/app.py
@@ -9,7 +9,7 @@ from sanic import Sanic, Request
 from sanic.exceptions import RequestTimeout, NotFound
 from sanic.response import json, HTTPResponse
 import os
-
+from sanic_openapi import swagger_blueprint
 from backend.exception.SpeechException import MissParameters
 from backend.model.ResponseBody import ResponseBody
 from backend.route.RecognitionRoute import recognition_route
@@ -48,7 +48,34 @@ async def notfound(request, exception):
     return json(response, 404)
 
 
+@app.middleware("request")
+def cors_middle_req(request: Request):
+    """路由需要启用OPTIONS方法"""
+    if request.method.lower() == 'options':
+        allow_headers = [
+            'Authorization',
+            'content-type'
+        ]
+        headers = {
+            'Access-Control-Allow-Methods':
+                ', '.join(request.app.router.get_supported_methods(request.path)),
+            'Access-Control-Max-Age': '86400',
+            'Access-Control-Allow-Headers': ', '.join(allow_headers),
+        }
+        return HTTPResponse('', headers=headers)
 
+
+@app.middleware("response")
+def cors_middle_res(request: Request, response: HTTPResponse):
+    """跨域处理"""
+    allow_origin = '*'
+    response.headers.update(
+        {
+            'Access-Control-Allow-Origin': allow_origin,
+        }
+    )
+
+app.blueprint(swagger_blueprint)
 app.blueprint(recognition_route)
 
 if __name__ == '__main__':
